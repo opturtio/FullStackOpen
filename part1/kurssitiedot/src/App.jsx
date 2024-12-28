f65d77f661eca5be8460902e9e9b70334fedbc17
opturtio <olli-pekka.turtio@helsinki.fi>
2024-12-28 17:49:14 +0200

    Update App.jsx

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -42,7 +42,7 @@ const Part = ({ part, exercises }) => {
   )
 }
 
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
   console.log(parts)
   return (
     <div>
@@ -53,7 +53,7 @@ const Content = ({parts}) => {
   )
 }
 
-const Total = ({parts}) => {
+const Total = ({ parts }) => {
   return (
     <div>
       <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
