70904b23665ac6fa9eeaf1328105f50a9cb9cdad
opturtio <olli-pekka.turtio@helsinki.fi>
2024-12-30 00:49:49 +0200

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,14 +6,18 @@ const Button = ({ label, handleClick }) => (
   <button onClick={handleClick}>{label}</button>
 )
 
-const ShowResult = ({ category, count }) => (
-  <p>{category} {count}</p>
+const ShowResult = ({ category, value }) => (
+  <p>{category} {value}</p>
 )
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = total > 0 ? ((good - bad) / total) : 0
+  const positive = total > 0 ? ((good / total) * 100) : 0
+
 
   const increaseGood = () => {
     setGood(good + 1)
@@ -34,9 +38,12 @@ const App = () => {
       <Button label="Neutral" handleClick={increaseNeutral} />
       <Button label="Bad" handleClick={increaseBad} />
       <Header text='Statistics' />
-      <ShowResult category='Good' count={good} />
-      <ShowResult category='Neutral' count={neutral} />
-      <ShowResult category='Bad' count={bad} />
+      <ShowResult category='Good' value={good} />
+      <ShowResult category='Neutral' value={neutral} />
+      <ShowResult category='Bad' value={bad} />
+      <ShowResult category="All" value={total} />
+      <ShowResult category="Average" value={average} />
+      <ShowResult category="Positive" value={`${positive}%`} />
       
     </div>
   )
