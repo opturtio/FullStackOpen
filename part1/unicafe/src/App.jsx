b6c878eec98a3f7bca8a7de8ab70c4e1bb25454b
opturtio <olli-pekka.turtio@helsinki.fi>
2024-12-30 01:41:30 +0200

    1.11: unicafe step6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,43 +6,53 @@ const Button = ({ label, handleClick }) => (
   <button onClick={handleClick}>{label}</button>
 )
 
-const Statistics = ({ category, value }) => (
-  <p>{category} {value}</p>
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td style={{ paddingRight: '10px' }}>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
+
+const Statistics = (props) => {
+  if (props.total === 0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <div>
+          <StatisticLine text="Good" value={props.good} />
+          <StatisticLine text="Neutral" value={props.neutral} />
+          <StatisticLine text="Bad" value={props.bad} />
+          <StatisticLine text="All" value={props.total} />
+          <StatisticLine text="Average" value={props.average.toFixed(1)} />
+          <StatisticLine text="Positive" value={`${props.positive.toFixed(1)} %`} />
+    </div>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
   const total = good + neutral + bad
-  const average = total > 0 ? ((good - bad) / total) : 0
-  const positive = total > 0 ? ((good / total) * 100) : 0
-
-  const increaseGood = () => {
-    setGood(good + 1)
-  }
-
-  const increaseNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const increaseBad = () => {
-    setBad(bad + 1)
-  }
+  const average = total > 0 ? (good - bad) / total : 0
+  const positive = total > 0 ? (good / total) * 100 : 0
 
   return (
     <div>
-      <Header text='Give Feedback' />
-      <Button label="Good" handleClick={increaseGood} />
-      <Button label="Neutral" handleClick={increaseNeutral} />
-      <Button label="Bad" handleClick={increaseBad} />
-      <Header text='Statistics' />
-      <Statistics category='Good' value={good} />
-      <Statistics category='Neutral' value={neutral} />
-      <Statistics category='Bad' value={bad} />
-      <Statistics category="All" value={total} />
-      <Statistics category="Average" value={average} />
-      <Statistics category="Positive" value={`${positive}%`} />
+      <Header text="Give Feedback" />
+      <Button label="Good" handleClick={() => setGood(good + 1)} />
+      <Button label="Neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button label="Bad" handleClick={() => setBad(bad + 1)} />
+      <Header text="Statistics" />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
   )
 }
