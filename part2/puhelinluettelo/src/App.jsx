ac1bbe98547fc6ca8773fd6a368ad7bb3df2dd8d
opturtio <olli-pekka.turtio@helsinki.fi>
2025-01-29 20:45:38 +0200

    2.11: puhelinluettelo step6

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const initialPersons = [
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]
-
-  const [persons, setPersons] = useState(initialPersons)
+  const [persons, setPersons] = useState([])
   const [newChar, setNewChar] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [personsToShow, setPersonsToShow] = useState(initialPersons)
+  const [personsToShow, setPersonsToShow] = useState([])
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setPersonsToShow(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   const changeChar = (event) => {
     setNewChar(event.target.value)
