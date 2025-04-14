1845b9f700b306f948d8340df5668427be0a9797
opturtio <olli-pekka.turtio@helsinki.fi>
2025-04-14 22:58:38 +0300

    3.7.-3.8

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
 app.use(express.json())
+
+morgan.token('body', (req) => {
+  return req.method === 'POST' && Object.keys(req.body).length ? JSON.stringify(req.body) : ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const date = new Date()
 
