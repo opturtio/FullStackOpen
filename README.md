30c5c81af4c2b203f14dc5b283295b33a8e03d38
opturtio <olli-pekka.turtio@helsinki.fi>
2025-05-26 21:21:40 +0300

    9.1-9.2

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # FullStackOpen
-
-### Phone book app
-
-#### THE APP CAN BE FOUND FROM RENDER
-[https://phonebook-bud0.onrender.com](https://phonebook-bud0.onrender.com)
---
+++ part9/exerciseCalculator.ts
@@ -0,0 +1,45 @@
+interface Info { 
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+const sum = (numbers: number[]): number => numbers.reduce((acc, num) => acc + num, 0);
+
+const createRateDesc = (rating: number): string => {
+  switch(rating) {
+    case 1:
+      return 'try more';
+    case 2:
+      return 'not too bad but could be better';
+    case 3:
+      return 'great job';
+    default:
+      throw new Error('Something went wrong');
+  }
+}
+
+const calculateExercises = (exerciseHours: number[], target: number): Info => {
+  const periodLength = exerciseHours.length;
+  const trainingDays = exerciseHours.filter(d => d != 0).length;
+  const average = sum(exerciseHours)/periodLength
+  const success = average > target ? true : false;
+  const rating = average >= 2.5 ? 3 : average >= 1.6 ? 2 : 1;
+  const ratingDescription = createRateDesc(rating);
+
+  return {
+    periodLength: periodLength,
+    trainingDays: trainingDays,
+    success: success,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: target,
+    average: average
+    }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
