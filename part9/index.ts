396cc34150edb0659d0d0c2d9fbe5afd50867915
opturtio <olli-pekka.turtio@helsinki.fi>
2025-05-30 18:17:51 +0300

    9.7 WebExercises

--- part9/index.ts
+++ part9/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/hello', (_, res) => {
     res.send('Hello Full Stack!');
@@ -24,9 +27,25 @@ app.get("/bmi", (req, res) => {
   });
 });
 
+app.post("/exercises", (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { daily_exercises, target } = req.body;
+
+    if (!daily_exercises || !target) {
+        res.status(400).send({ error: "missing parameters" });
+    }
+    
+    if (!Array.isArray(daily_exercises) || !daily_exercises.every((n) => typeof n === "number") || typeof target !== "number") {
+        res.status(400).send({ error: "malformatted parameters" });
+    }
+    
+    const exercises: number[] = daily_exercises.map(Number) as number[];
+    const result = calculateExercises(exercises, Number(target));
+    res.status(200).json({ result });
+});
+
 const PORT = 3003;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
