4673d4cbab55d3e7502a40c0555c3b1243bcab3f
opturtio <olli-pekka.turtio@helsinki.fi>
2025-09-21 22:10:34 +0300

    Create model and controller

--- part4/blogilistasovellus/server/app.js
+++ part4/blogilistasovellus/server/app.js
@@ -1,36 +1,14 @@
 const config = require('./utils/config')
-
+const blogRouter = require('./controllers/blog')
 const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
 
-mongoose.set('strictQuery', false)
 mongoose.connect(config.mongoUrl)
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blog', blogRouter)
 
 module.exports = app
